643d17a544c8c7ef58fb3e55845143d6b388f5d2
Fernando Passos <fernandopassos0825@passos.net.br>
2025-08-03 21:52:36 +0000

    mudando o botao domingo

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Add New Task
+        Incluir nova tarefa
       </button>
       
       <Modal
